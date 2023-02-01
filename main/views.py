049947b4e44d8fed0746a00e20b7d52daefa17f7
Michael Camerlynck <71758112+MichaelCamerlynck@users.noreply.github.com>
2023-02-01 15:54:54 +0100

    Footer and Navigation Added

--- main/views.py
+++ main/views.py
@@ -11,5 +11,5 @@ class IndexView(TemplateView):
    def get_context_data(self, **kwargs):
        context = super().get_context_data(**kwargs)
        context["data"] = "Site under construction, come back soon..."
-       context["title"] = "Site Under Construction"
+       context["title"] = "Home"
        return context
