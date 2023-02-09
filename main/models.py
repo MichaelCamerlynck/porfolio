b617b26479c85ef17b2ddfd1657495c8cd57c2c4
Michael Camerlynck <71758112+MichaelCamerlynck@users.noreply.github.com>
2023-02-09 18:12:13 +0100

    Project Overview

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=256)
     date = models.PositiveBigIntegerField()
+    scope = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
